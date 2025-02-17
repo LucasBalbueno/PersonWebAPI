e2de9545d96b481108e08ce4f51da68b793dcf06
LucasBalbueno <balbuenolucas04@gmail.com>
2025-02-17 09:42:28 -0300

    Inicializando API

---
+++ Person/Routes/PersonRoute.cs
@@ -0,0 +1,13 @@
+namespace Person.Routes;
+
+// Nesse arquivo ficará todas as rotas e métodos HTTPS da nossa aplicação;
+// Essa classe pode ser static pois não iremos instanciar ela;
+public static class PersonRoute
+{
+    // Quando a classe é static os métodos dela também devem ser
+    // Aqui usamos Extension Methods
+    public static void PersonRoutes(this WebApplication app)
+    {
+        app.MapGet("person", () => "Hello World!");
+    }
+}
